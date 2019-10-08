470583d06025104325195a9cbb492a3e957b6993
yanlele <331393627@qq.com>
2019-10-09 01:15:24 +0800

    [+]打印sql

--- src/main/java/com/spider/data/server/spider/controller/StoreList.java
+++ src/main/java/com/spider/data/server/spider/controller/StoreList.java
@@ -2,12 +2,15 @@ package com.spider.data.server.spider.controller;
 
 
 import com.spider.data.server.spider.data.BaseResponse;
+import com.spider.data.server.spider.entity.StoreEntity;
 import com.spider.data.server.spider.service.StoreService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/store")
@@ -21,11 +24,11 @@ public class StoreList {
             @RequestParam(name = "page", defaultValue = "1") String page,
             @RequestParam(name = "size", defaultValue = "20") String size
     ) {
-//        List<StoreEntity> list = storeService.findAll(query, page, size);
-//        if (list != null) {
-//            System.out.println(list.size());
-//            return BaseResponse.responseSuccess(list, "请求成功");
-//        }
+        List<StoreEntity> list = storeService.findAll(query, page, size);
+        if (list != null) {
+            System.out.println(list.size());
+            return BaseResponse.responseSuccess(list, "请求成功");
+        }
         return BaseResponse.responseError("请求失败");
     }
 }
