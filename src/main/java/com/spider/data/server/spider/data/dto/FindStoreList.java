c80e81b7f1ce1938fbf81d8aa3051366166d9afd
yanlele <331393627@qq.com>
2019-10-10 00:57:31 +0800

    [+]整改研究内容

--- src/main/java/com/spider/data/server/spider/data/dto/FindStoreList.java
+++ src/main/java/com/spider/data/server/spider/data/dto/FindStoreList.java
@@ -1,10 +1,13 @@
 package com.spider.data.server.spider.data.dto;
 
+import com.alibaba.fastjson.annotation.JSONField;
 import lombok.Data;
 
 @Data
 public class FindStoreList {
+    @JSONField
     private String query;
 
+    @JSONField
     private String order;
 }
